c98b3a8e992103898a0cd70c2322e5b7d82743bf
Ralph Kube <ralph_kube@gmx.net>
2022-01-27 09:57:45 -0500

    Fixing ordering in utils

--- src/utils.jl
+++ src/utils.jl
@@ -12,14 +12,14 @@ function save_images(x_fake, filename)
     img_width, img_height, ch, num_images = size(x_fake)
     # Number of images per row
     num_rows = 10
-    num_cols = num_images ÷ 10
+    num_cols = num_images ÷ num_rows
     # img_arr will hold the output image
     img_array = zeros(RGB, img_height * num_rows, img_width * num_cols) 
     # Put the samples, addressed through the last dimension of X_cpu, column-wise into img_arr
     X_cpu = x_fake |> cpu;
-    for row in 1:num_rows
-       for col in 1:num_cols
-           img_array[(row - 1) * img_width + 1:row * img_height, (col - 1) * img_width + 1: col * img_height] = colorview(RGB, permutedims(X_cpu[:, :, :, (row -1)* num_cols + col], (3, 2, 1)))
+    for col in 1:num_cols
+       for row in 1:num_rows
+           img_array[(col - 1) * img_width + 1:col * img_width, (row - 1) * img_height + 1: row * img_height] = colorview(RGB, permutedims(X_cpu[:, :, :, (row -1)* num_cols + col], (3, 2, 1)))
        end
    end
    img_array = map(clamp01nan, img_array)
